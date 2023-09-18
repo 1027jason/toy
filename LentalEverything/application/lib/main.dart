aa73de964c5deec1df33a9d6b8c169c325b8c019
넌할수있어 <1027jason85@gmail.com>
2023-09-18 14:46:39 +0900

    Daily auto-commit

--- LentalEverything/application/lib/main.dart
+++ LentalEverything/application/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:application/HomeScreen/homescreen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // Firebase 패키지를 import 합니다.
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Flutter 바인딩을 확실히 초기화합니다.
-  await Firebase.initializeApp(); // Firebase를 초기화합니다.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Firebase를 초기화합니다.
   runApp(const MyApp());
 }
 
